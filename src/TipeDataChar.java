3a1bf99f37baedfb21bdf5f0bbc94d6c51089bbb
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-12-03 02:54:57 +0700

    Add Tipe data Karakter

---
+++ src/TipeDataChar.java
@@ -0,0 +1,13 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char d = 'D';
+        char y = 'Y';
+        char a = 'A';
+        char z = 'Z';
+
+        System.out.print(d);
+        System.out.print(y);
+        System.out.print(a);
+        System.out.print(z);
+    }
+}
