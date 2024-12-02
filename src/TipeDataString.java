40d425ab03f54e0cf082d2c32be908b8c6cd23dc
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-12-03 03:01:55 +0700

    Add Tipe data Boolean dan String

---
+++ src/TipeDataString.java
@@ -0,0 +1,12 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Dyaz";
+        String lastName = "Amrullah";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        System.out.println(fullName);
+    }
+}
